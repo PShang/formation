a4b73b2c7c66e2fed41a6eb24b57b01d43d6ddcf
Etienne Bohain <EtienneTiti1990@gmail.com>
2016-04-28 13:09:42 +0200

    Suppression demande de nouveau code pizza lors d'une modification.

--- pizzeria-console-objet/src/fr/pizzeria/ihm/menu/option/ModifierPizzaOptionMenu.java
+++ pizzeria-console-objet/src/fr/pizzeria/ihm/menu/option/ModifierPizzaOptionMenu.java
@@ -19,20 +19,18 @@ public class ModifierPizzaOptionMenu extends OptionMenu {
 	public boolean execute() {
 		System.out.println("Mise à jour d’une pizza");
 		new ListerPizzaOptionMenu(pizzaDao).execute();
-		System.out.println("Veuillez choisir la pizza à modifier.");
+		System.out.println("Veuillez choisir le code la pizza à modifier.");
 		System.out.println("(99 pour abandonner).");
-		String oldCode = scan.next();
-		if (oldCode.equals("99")) {
+		String code = scan.next();
+		if (code.equals("99")) {
 			return false;
 		}
-		System.out.println("Veuillez saisir le code");
-		String code = scan.next();
 		System.out.println("Veuillez saisir le nom (sans espace)");
 		String name = scan.next();
 		System.out.println("Veuillez saisir le prix");
 		double price = scan.nextDouble();
-		if (!pizzaDao.updatePizza(oldCode, new Pizza(code, name, price))) {
-			System.err.println("Erreur : La pizza avec le code " + oldCode + " n'existe pas.");
+		if (!pizzaDao.updatePizza(code, new Pizza(code, name, price))) {
+			System.err.println("Erreur : La pizza avec le code " + code + " n'existe pas.");
 		}
 		System.out.println();
 		return false;
