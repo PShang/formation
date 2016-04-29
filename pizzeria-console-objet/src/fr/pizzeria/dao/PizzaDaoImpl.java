d3982c5178249764d7e00492e218c7b1c1064612
Etienne Bohain <EtienneTiti1990@gmail.com>
2016-04-29 11:45:50 +0200

    Remplacement de la List par une Map pour les pizzas

--- pizzeria-console-objet/src/fr/pizzeria/dao/PizzaDaoImpl.java
+++ pizzeria-console-objet/src/fr/pizzeria/dao/PizzaDaoImpl.java
@@ -1,77 +1,55 @@
 package fr.pizzeria.dao;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import fr.pizzeria.exception.DaoException;
 import fr.pizzeria.model.Pizza;
 
 public class PizzaDaoImpl implements IPizzaDao {
 
-	private List<Pizza> pizzas;
+	private Map<String, Pizza> pizzas;
 
 	public PizzaDaoImpl() {
-		pizzas = new ArrayList<Pizza>();
-		pizzas.add(new Pizza("PEP", "Pépéroni", 12.50));
-		pizzas.add(new Pizza("MAR", "Margherita", 14.00));
-		pizzas.add(new Pizza("REI", "La Reine", 11.50));
-		pizzas.add(new Pizza("FRO", "La 4 fromages", 12.00));
-		pizzas.add(new Pizza("CAN", "La cannibale", 12.50));
-		pizzas.add(new Pizza("SAV", "La savoyarde", 13.00));
-		pizzas.add(new Pizza("ORI", "L'orientale", 13.50));
-		pizzas.add(new Pizza("IND", "L'indienne", 14.00));
-	}
-
-	/**
-	 * Vérifie si la pizza existe.
-	 *
-	 * @param code Le code de la pizza à chercher.
-	 * @return {@code true} si la pizza existe.
-	 */
-	private boolean pizzaExists(String code) {
-		for (Pizza pizza : pizzas) {
-			if (pizza.getCode().equals(code))
-				return true;
-		}
-		return false;
+		pizzas = new HashMap<String, Pizza>();
+		pizzas.put("PEP", new Pizza("PEP", "Pépéroni", 12.50));
+		pizzas.put("MAR", new Pizza("MAR", "Margherita", 14.00));
+		pizzas.put("REI", new Pizza("REI", "La Reine", 11.50));
+		pizzas.put("FRO", new Pizza("FRO", "La 4 fromages", 12.00));
+		pizzas.put("CAN", new Pizza("CAN", "La cannibale", 12.50));
+		pizzas.put("SAV", new Pizza("SAV", "La savoyarde", 13.00));
+		pizzas.put("ORI", new Pizza("ORI", "L'orientale", 13.50));
+		pizzas.put("IND", new Pizza("IND", "L'indienne", 14.00));
 	}
 
 	@Override
 	public List<Pizza> findAllPizzas() {
-		return new ArrayList<Pizza>(pizzas);
+		return new ArrayList<Pizza>(pizzas.values());
 	}
 
 	@Override
 	public void saveNewPizza(Pizza pizza) throws DaoException {
-		if (pizzaExists(pizza.getCode())) {
+		if (pizzas.containsKey(pizza.getCode())) {
 			throw new DaoException();
 		}
-		pizzas.add(pizza);
+		pizzas.put(pizza.getCode(), pizza);
 	}
 
 	@Override
 	public void updatePizza(String codePizza, Pizza pizza) throws DaoException {
-		if (!pizzaExists(pizza.getCode())) {
+		if (!pizzas.containsKey(pizza.getCode())) {
 			throw new DaoException();
 		}
-		for (int i = 0; i < pizzas.size(); ++i) {
-			if (pizzas.get(i).getCode().equals(codePizza)) {
-				pizzas.set(i, pizza);
-			}
-		}
+		pizzas.put(codePizza, pizza);
 	}
 
 	@Override
 	public void deletePizza(String codePizza) throws DaoException {
-		if (!pizzaExists(codePizza)) {
+		if (!pizzas.containsKey(codePizza)) {
 			throw new DaoException();
 		}
-		Iterator<Pizza> it = pizzas.iterator();
-		while (it.hasNext()) {
-			if (it.next().getCode().equals(codePizza)) {
-				it.remove();
-			}
-		}
+		pizzas.remove(codePizza);
 	}
 }
