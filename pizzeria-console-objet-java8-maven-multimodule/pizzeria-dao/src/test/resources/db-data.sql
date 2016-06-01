1da066ed27cb342528a274a5d2a0930860cb1b89
Etienne Bohain <EtienneTiti1990@gmail.com>
2016-06-01 17:35:54 +0200

    Test Spring jdbc template (en cours)

---
+++ pizzeria-console-objet-java8-maven-multimodule/pizzeria-dao/src/test/resources/db-data.sql
@@ -0,0 +1,12 @@
+INSERT INTO `pizza` (`id`, `categorie`, `code`, `nom`, `prix`, `url_image`) VALUES
+(1, 'VIANDE', 'ZZZ', 'Aaa', '3.21', 'http://placehold.it/150x150'),
+(2, 'VIANDE', 'IND', 'L''indienne', '14.00', 'http://placehold.it/150x150'),
+(3, 'VIANDE', 'ORI', 'L''orientale', '13.50', 'http://placehold.it/150x150'),
+(4, 'SANS_VIANDE', 'FRO', 'La 4 fromages', '12.00', 'http://placehold.it/150x150'),
+(5, 'VIANDE', 'REI', 'La Reine', '11.50', 'http://placehold.it/150x150'),
+(6, 'VIANDE', 'CAN', 'La cannibale', '12.50', 'http://placehold.it/150x150'),
+(7, 'POISSON', 'SAU', 'La saumonetta', '15.50', 'http://placehold.it/150x150'),
+(8, 'VIANDE', 'SAV', 'La savoyarde', '13.00', 'http://placehold.it/150x150'),
+(9, 'SANS_VIANDE', 'MAR', 'Margherita', '14.00', 'http://placehold.it/150x150'),
+(10, 'VIANDE', 'PEP', 'Pépéroni', '12.50', 'http://placehold.it/150x150'),
+(11, 'POISSON', 'AAA', 'Test', '31.51', 'http://placehold.it/150x150');
---
+++ pizzeria-console-objet-java8-maven-multimodule/pizzeria-dao/src/test/resources/db-schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `pizza` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `categorie` varchar(255) NOT NULL,
+  `code` varchar(8) NOT NULL,
+  `nom` varchar(32) NOT NULL,
+  `prix` decimal(19,2) NOT NULL,
+  `url_image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_m169cbpctqeb9bc04mkr6nw7n` (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
